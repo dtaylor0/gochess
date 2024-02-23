5f39771dba9242cd8e10e597d102698f12a7077e
Drew Taylor <drewtaylorm@gmail.com>
2024-02-23 08:21:07 -0500

    board looks ok

--- server.go
+++ server.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/html/v2"
+	// "math/rand"
 	"log"
 	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
+
+	// perspective := rand.Intn(1)
+	// position := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+	// move := "e4"
+
 	logger := log.New(os.Stdout, "", 0)
 	engine := html.New("./views", ".html")
 
@@ -16,14 +23,43 @@ func main() {
 	app.Static("/static", "./static")
 
 	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("game", fiber.Map{})
+	})
+
+	app.Get("/board", func(c *fiber.Ctx) error {
 		return c.Render("board", fiber.Map{})
 	})
 
-	app.Get("/game", func(c *fiber.Ctx) error {
-		return c.Render("game", fiber.Map{})
+	app.Get("/board/update", func(c *fiber.Ctx) error {
+		payload := struct {
+			FEN         string `json:"fen"`
+			Perspective string `json:"perspective"`
+			Move        string `json:"move"`
+		}{}
+		if err := c.BodyParser(&payload); err != nil {
+			return err
+		}
+
+		// updatedBoard, err := updateBoard(
+		// 	payload.Move,
+		// 	payload.FEN,
+		// 	payload.Perspective,
+		// )
+        // if err != nil {
+        //     return err
+        // }
+
+		return c.Render("board", fiber.Map{})
 	})
 	logger.Println("Hello from logger")
 
-    app.Listen(":3000")
+	app.Listen(":3000")
 
+}
+
+func createBoard(position string, perspective int) {
+}
+
+func applyMove(move string, position string, perspective int) (string, error) {
+	return "", nil
 }
