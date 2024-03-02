e9370e0e0a2ba859010fc1125e80b4ffe80199f5
Drew Taylor <drewtaylorm@gmail.com>
2024-03-01 21:01:13 -0500

    actually rendering from FEN now

--- server.go
+++ server.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
 	"os"
-	"slices"
 	"strings"
 	"unicode"
 
@@ -16,7 +16,6 @@ type BoardRows [][]map[string]string
 
 func main() {
 
-	fen_start := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 	// move := "e4"
 
 	logger := log.New(os.Stdout, "", 0)
@@ -31,83 +30,9 @@ func main() {
 	})
 
 	app.Get("/board", func(c *fiber.Ctx) error {
-		perspective := rand.Intn(1)
-		board_input := createBoard(fen_start, perspective)
-		board_input = BoardRows{
-			{
-				{"Piece": "r", "Id": "ra8"},
-				{"Piece": "n", "Id": "nb8"},
-				{"Piece": "b", "Id": "bc8"},
-				{"Piece": "q", "Id": "qd8"},
-				{"Piece": "k", "Id": "ke8"},
-				{"Piece": "b", "Id": "bf8"},
-				{"Piece": "n", "Id": "ng8"},
-				{"Piece": "r", "Id": "rh8"},
-			}, {
-				{"Piece": "p", "Id": "pa7"},
-				{"Piece": "p", "Id": "pb7"},
-				{"Piece": "p", "Id": "pc7"},
-				{"Piece": "p", "Id": "pd7"},
-				{"Piece": "p", "Id": "pe7"},
-				{"Piece": "p", "Id": "pf7"},
-				{"Piece": "p", "Id": "pg7"},
-				{"Piece": "p", "Id": "ph7"},
-			}, {
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-			}, {
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-			}, {
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-			}, {
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-				{"Piece": "", "Id": ""},
-			}, {
-				{"Piece": "P", "Id": "pa2"},
-				{"Piece": "P", "Id": "pb2"},
-				{"Piece": "P", "Id": "pc2"},
-				{"Piece": "P", "Id": "pd2"},
-				{"Piece": "P", "Id": "pe2"},
-				{"Piece": "P", "Id": "pf2"},
-				{"Piece": "P", "Id": "pg2"},
-				{"Piece": "P", "Id": "ph2"},
-			}, {
-				{"Piece": "R", "Id": "Ra1"},
-				{"Piece": "N", "Id": "Nb1"},
-				{"Piece": "B", "Id": "Bc1"},
-				{"Piece": "Q", "Id": "Qd1"},
-				{"Piece": "K", "Id": "Ke1"},
-				{"Piece": "B", "Id": "Bf1"},
-				{"Piece": "N", "Id": "Ng1"},
-				{"Piece": "R", "Id": "Rh1"},
-			},
-		}
+		perspective := rand.Intn(2)
+		fen_start := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+        board_input := createBoard(fen_start, perspective)
 		return c.Render("board", fiber.Map{"Rows": board_input})
 	})
 
@@ -132,8 +57,8 @@ func main() {
 
 		return c.Render("board", fiber.Map{})
 	})
-	logger.Println("Hello from logger")
 
+	logger.Println("Hello from logger")
 	app.Listen(":3000")
 
 }
@@ -142,28 +67,40 @@ func applyMove(move string, fen string, perspective int) (string, error) {
 	return "", nil
 }
 
+func reverseString(s string) string {
+	res := ""
+	for _, r := range s {
+		res = strings.Join([]string{string(r), res}, "")
+	}
+	return res
+}
+
 func createBoard(fen string, perspective int) BoardRows {
 	var rows BoardRows
 	var currRow []map[string]string
-	if perspective == 0 {
-		for i, r := range fen {
-			if unicode.IsDigit(r) {
-				repeat := int(r - '0')
-				for repeat > 0 {
-					currRow = append(currRow, map[string]string{"Piece": "", "Id": ""})
-				}
-			} else if r == '/' {
-				rows = append(rows, currRow)
-				clear(currRow)
-			} else {
-				currRow = append(
-					currRow,
-					map[string]string{
-						"Piece": string([]rune{r}),
-						"Id":    strings.Join(string([]rune{r}), string(i)),
-					},
-				)
+	if perspective == 1 {
+		fen = reverseString(fen)
+	}
+	for i, r := range fen {
+		if unicode.IsDigit(r) {
+			repeat := int(r - '0')
+			for repeat > 0 {
+				currRow = append(currRow, map[string]string{"Piece": "", "Id": ""})
+				repeat--
 			}
+		} else if r == '/' {
+			rows = append(rows, currRow)
+			currRow = nil
+		} else {
+			currRow = append(
+				currRow,
+				map[string]string{
+					"Piece": string([]rune{r}),
+					"Id":    strings.Join([]string{string(r), fmt.Sprint(i)}, ""),
+				},
+			)
 		}
 	}
+    rows = append(rows, currRow)
+	return rows
 }
